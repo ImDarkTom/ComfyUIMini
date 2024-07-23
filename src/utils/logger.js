86cba46d4ec9bd40e4b1c67226f748faf9a41f8b
ImDarkTom <darktomg21@gmail.com>
2024-07-23 23:00:10 +0100

    ✨ feat: image placeholders

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,17 +1,42 @@
+const config = require('../../config.json');
+
+let logPreprends = {
+    "info": "INFO",
+    "success": "SUCCESS",
+    "warn": "WARN",
+    "optional": "OPTIONAL",
+};
+
+if (config.use_log_emojis) {
+    logPreprends = {
+        "info": "ℹ",
+        "success": "✅",
+        "warn": "⚠",
+        "optional": "OPTIONAL ℹ",
+    };
+}
+
 function logInfo(message) {
-    console.log(`[INFO] ${message}`);
+    console.log(`[${logPreprends.info}] ${message}`);
 }
 
 function logSuccess(message) {
-    console.log(`[SUCCESS] ${message}`);
+    console.log(`[${logPreprends.success}] ${message}`);
 }
 
 function logWarning(message) {
-    console.log(`[WARN] ${message}`);
+    console.log(`[${logPreprends.warn}] ${message}`);
+}
+
+function optionalLog(option, message) {
+    if (option === true) {
+        console.log(`[${logPreprends.optional}] ${message}`);
+    }
 }
 
 module.exports = {
     logInfo,
     logSuccess,
-    logWarning
+    logWarning,
+    optionalLog
 }
