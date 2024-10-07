a52f33eb827a976f4859131ccb26a6128a7ddade
ImDarkTom <darktomg21@gmail.com>
2024-10-07 11:08:04 +0100

    💄 ui: toggle pagination controls button

--- src/public/js/pages/gallery.js
+++ src/public/js/pages/gallery.js
@@ -1,7 +1,27 @@
 const pageInput = document.getElementById('page-input');
+const paginationContainer = document.querySelector('.pagination-container');
 
 pageInput.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
         window.location.href = `?page=${pageInput.value}`;
     }
 });
+
+export function togglePaginationControls() {
+    if (paginationContainer.classList.contains('hidden')) {
+        paginationContainer.classList.remove('hidden');
+
+        setTimeout(() => {
+            paginationContainer.classList.remove('slide-down');
+        }, 300);
+
+    } else {
+        paginationContainer.classList.add('slide-down');
+
+        setTimeout(() => {
+            paginationContainer.classList.add('hidden');
+        }, 300);
+        
+        
+    }   
+}
