175a5316a413ca407a8e7749a88a1c1dc8d9cc73
ImDarkTom <darktomg21@gmail.com>
2024-10-12 19:51:33 +0100

    🔥 remove: hardcoded select options logic

--- src/routes/comfyUIRouter.js
+++ src/routes/comfyUIRouter.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const config = require('config');
 const { getHistory, getQueue, interruptGeneration, getImage } = require('../utils/comfyAPIUtils');
-const { getItemsForSelectType, loadSelectOptions } = require('../utils/selectOptionUtils');
 const { inputsInfoObject } = require('../utils/objectInfoUtils');
 
 const router = express.Router();
@@ -28,28 +26,6 @@ router.get('/image', async (req, res) => {
     res.set('Content-Type', imageResponse.headers['content-type']);
     res.set('Content-Length', imageResponse.headers['content-length']);
     res.send(imageResponse.data);
-});
-
-router.get('/selectoptions', async (req, res) => {
-    if (config.selectOptions.models.length === 0) {
-        console.info('Reloading select options...')
-        await loadSelectOptions();
-    }
-
-    res.json(config.selectOptions);
-});
-
-router.get('/selectoption/:selectType', async (req, res) => {
-    const selectType = req.params.selectType;
-
-    const selectTypeItems = await getItemsForSelectType(selectType);
-
-    if (selectTypeItems == []) {
-        res.send(`Select option ${selectType} is either invalid or empty.`).status(400);
-        return;
-    }
-
-    res.json(selectTypeItems);
 });
 
 router.get('/queue', async (req, res) => {
