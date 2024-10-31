b09f01b8f67b91cbd2ef197231749ad01cc5e350
ImDarkTom <darktomg21@gmail.com>
2024-10-31 02:31:30 +0000

    🧑‍💻 dx: centralised paths file

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
 import express from 'express';
 import http from 'http';
-import path from 'path';
+import config from 'config';
 
+// Makes sure that if there isn't a config file created, it is created before anything else
 import { initialiseConfig } from './utils/configInitialiser';
 initialiseConfig();
 
-import config from 'config';
+import paths from './utils/paths';
 import logger from './utils/logger';
 import { handleUpgrade } from './routes/wsRouter';
-
 import mainRouter from './routes/mainRouter';
 import comfyUIRouter from './routes/comfyUIRouter';
 import settingsRouter from './routes/settingsRouter';
+import { comfyUICheck } from './utils/comfyAPIUtils';
+import { serverWorkflowsCheck } from './utils/workflowUtils';
+import getLocalIp from './utils/localIp';
 
 const app = express();
 const server = http.createServer(app);
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', paths.views);
 
 app.use('/', mainRouter);
 app.use('/comfyui', comfyUIRouter);
 app.use('/setsetting', settingsRouter);
-app.use(express.static(path.join(__dirname, 'public')));
-
-server.on('upgrade', handleUpgrade);
-
-import { comfyUICheck } from './utils/comfyAPIUtils';
-import { serverWorkflowsCheck } from './utils/workflowUtils';
-import getLocalIp from './utils/localIp';
+app.use(express.static(paths.public));
 
 comfyUICheck();
 serverWorkflowsCheck();
+
+server.on('upgrade', handleUpgrade);
 
 server.listen(config.get('app_port'), '0.0.0.0', () => {
     logger.success(`Running on http://${getLocalIp()}:${config.get('app_port')}`);
--- src/utils/configInitialiser.ts
+++ src/utils/configInitialiser.ts
@@ -1,13 +1,16 @@
 import fs from 'fs';
 import path from 'path';
+import paths from './paths';
 
 function initialiseConfig() {
-    const configDir = path.join(__dirname, '..', '..', 'config', 'default.json');
+    const configDir = path.join(paths.config, 'default.json');
 
     if (!fs.existsSync(configDir)) {
         try {
-            fs.copyFileSync(path.join(__dirname, '..', '..', 'config', 'default.example.json'), configDir);
-            console.log('Creating config file, you may need to manually configure some options for full functionality.');
+            fs.copyFileSync(path.join(paths.config, 'default.example.json'), configDir);
+            console.log(
+                'Creating config file, you may need to manually configure some options for full functionality.'
+            );
         } catch (err) {
             console.error('Error when creating config file:', err);
             process.exit(1);
