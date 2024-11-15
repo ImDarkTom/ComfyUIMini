21b10899d7c6f0576f1e6096c3d91232bac8ec30
ImDarkTom <darktomg21@gmail.com>
2024-11-15 01:52:59 +0000

    🔨 scrips: update scripts and scripts folder

---
+++ scripts/update.bat
@@ -0,0 +1,44 @@
+@echo off
+
+:: Check if git is installed
+where git >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Git is not installed. Please install git and try again or manually download the files from the repository and update manually.
+    pause
+    exit /b
+)
+
+:: Check if npm is installed
+where npm >nul 2>nul
+if %errorlevel% neq 0 (
+    echo NPM is not installed. Please install NPM and Node.js and try again.
+    pause
+    exit /b
+)
+
+echo Downloading latest version...
+call git pull
+if %errorlevel% neq 0 (
+    echo Failed to pull latest version. Please check your internet connection and try again.
+    pause
+    exit /b
+)
+
+echo Updating dependencies...
+call npm install
+if %errorlevel% neq 0 (
+    echo Failed to update dependencies. Please check your internet connection and try again.
+    pause
+    exit /b
+)
+
+echo Building...
+call npm run build
+if %errorlevel% neq 0 (
+    echo Build failed. Check the console for more information.
+    pause
+    exit /b
+)
+
+echo Update complete. Run 'npm start' to launch the app
+pause
---
+++ scripts/update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Chech if git is installed
+if ! command -v git &> /dev/null
+then
+    echo "Git is not installed. Please install git and try again or manually download the files from the repository and update manually."
+    exit 1
+fi
+
+# Check if npm is installed
+if ! command -v npm &> /dev/null
+then
+    echo "NPM is not installed. Please install NPM and Node.js and try again."
+    exit 1
+fi
+
+echo "Downloading latest version..."
+git pull
+if [ $? -ne 0 ]; then
+    echo "Failed to pull latest version. Please check your internet connection and try again."
+    exit 1
+fi
+
+echo "Updating dependencies..."
+npm install
+if [ $? -ne 0 ]; then
+    echo "Failed to update dependencies. Please check your internet connection and try again."
+    exit 1
+fi
+
+echo "Building..."
+npm run build
+if [ $? -ne 0 ]; then
+    echo "Build failed. Check the console for more information."
+    exit 1
+fi
+
+echo "Update complete. Run 'npm start' to launch the app"
