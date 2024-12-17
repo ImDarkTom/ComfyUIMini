462f4fe7d9a6bce34dc854bef033235f6da77583
ImDarkTom <darktomg21@gmail.com>
2024-12-17 00:06:08 +0000

    🔥 remove: remove action bar from pages

--- src/client/public/js/pages/gallery.ts
+++ src/client/public/js/pages/gallery.ts
@@ -10,22 +10,3 @@ pageInput.addEventListener('keyup', (e) => {
         window.location.href = `?page=${pageInput.value}`;
     }
 });
-
-export function togglePaginationControls() {
-    if (paginationContainer.classList.contains('hidden')) {
-        paginationContainer.classList.remove('hidden');
-
-        setTimeout(() => {
-            paginationContainer.classList.remove('slide-down');
-        }, 300);
-
-    } else {
-        paginationContainer.classList.add('slide-down');
-
-        setTimeout(() => {
-            paginationContainer.classList.add('hidden');
-        }, 300);
-        
-        
-    }   
-}
--- src/client/public/js/scrollbuttons.ts
+++ src/client/public/js/scrollbuttons.ts
@@ -1,7 +1,25 @@
-import { scrollToBottom, scrollToTop } from "./common/scroll.js";
+import { scrollToBottom, scrollToTop } from './common/scroll.js';
 
 const scrollTopButton = document.getElementById('scroll-top') as HTMLElement;
 const scrollBottomButton = document.getElementById('scroll-bottom') as HTMLElement;
 
 scrollTopButton.addEventListener('click', () => scrollToTop());
 scrollBottomButton.addEventListener('click', () => scrollToBottom());
+
+document.addEventListener('scroll', () => {
+    const totalPageHeight = document.body.scrollHeight;
+
+    const scrollPoint = window.scrollY + window.innerHeight;
+
+    if (scrollPoint >= totalPageHeight) {
+        scrollBottomButton.style.visibility = 'hidden';
+    } else {
+        scrollBottomButton.style.visibility = 'visible';
+    }
+
+    if (scrollPoint <= window.innerHeight) {
+        scrollTopButton.style.visibility = 'hidden';
+    } else {
+        scrollTopButton.style.visibility = 'visible';
+    }
+});
